e19416cc133f61470b200dfa2f992d6ffdf2ab7a
Elvis Garcia <elvisgarcia93@gmail.com>
2022-03-10 15:20:53 -0500

    Update Server to include no Solution

--- server.js
+++ server.js
@@ -4,19 +4,7 @@ const port = 3000;
 const {Restaurant, Menu, Item} = require("./models/index")
 const {sequelize} = require("./db")
 
-app.get("/restaurants", async (req, res) => {
-    const restaurantWithItems = await Restaurant.findAll({
-        include: Menu,
-        include: [{
-            model: Menu,
-            include: [{
-                model: Item
-            }]
-        }]
-    });
-    res.json(restaurantWithItems);
-})
-
+//TODO: 
 
 app.listen(port, () => {
     sequelize.sync()
